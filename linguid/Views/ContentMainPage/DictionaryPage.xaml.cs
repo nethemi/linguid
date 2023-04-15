25100b34176c918352bc936d36cfc515269f4f8a
Leleria <valeriamarynich1004@gmail.com>
2023-04-15 22:40:50 +0700

    searchBar

--- linguid/Views/ContentMainPage/DictionaryPage.xaml.cs
+++ linguid/Views/ContentMainPage/DictionaryPage.xaml.cs
@@ -16,5 +16,13 @@ namespace linguid.Views.ContentMainPage
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void SearchBarTextChanged(object sender, TextChangedEventArgs e)
+        {
+			recentlyViewed.IsVisible = !recentlyViewed.IsVisible;
+			dictionatyView.ItemsSource = null;
+			if (searchBar.Text != "")
+				dictionatyView.ItemsSource = (await App.Database.GetMeaningWithChildren()).Where(z => z.dictionary.fkLanguage == 1 && (z.dictionary.Item.StartsWith(e.NewTextValue) || z.transcription.TranscriptionItem.StartsWith(e.NewTextValue) || z.dictionaryRu.ItemRu.StartsWith(e.NewTextValue)));
+		}
+    }
 }
