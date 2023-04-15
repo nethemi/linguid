6b5d9ede32a94b5355040e7c698e836a0707e2ac
Анжела Санникова <sannikova.anzhela.01@outlook.com>
2023-04-16 01:36:05 +0700

    words

--- linguid/Views/ContentMainPage/DictionaryPage.xaml.cs
+++ linguid/Views/ContentMainPage/DictionaryPage.xaml.cs
@@ -21,16 +21,33 @@ namespace linguid.Views.ContentMainPage
         private async void SearchBarTextChanged(object sender, TextChangedEventArgs e)
         {
             var userLogin = Thread.CurrentPrincipal.Identity.Name;
-            foreach (var user in await App.Database.GetUserAsync())
+			if (userLogin != "")
+			{
+                foreach (var user in await App.Database.GetUserAsync())
+                {
+                    if (userLogin == user.UserLogin)
+                    {
+                        recentlyViewed.IsVisible = !recentlyViewed.IsVisible;
+                        dictionatyView.ItemsSource = null;
+                        if (searchBar.Text != "")
+                            dictionatyView.ItemsSource = (await App.Database.GetMeaningWithChildren()).Where(z => z.dictionary.fkLanguage == user.fkLanguage && (z.dictionary.Item.StartsWith(e.NewTextValue) || z.transcription.TranscriptionItem.StartsWith(e.NewTextValue) || z.dictionaryRu.ItemRu.StartsWith(e.NewTextValue)));
+                    }
+                }
+            }
+            else
             {
-				if (userLogin == user.UserLogin)
-				{
-					recentlyViewed.IsVisible = !recentlyViewed.IsVisible;
-					dictionatyView.ItemsSource = null;
-					if (searchBar.Text != "")
-						dictionatyView.ItemsSource = (await App.Database.GetMeaningWithChildren()).Where(z => z.dictionary.fkLanguage == user.fkLanguage && (z.dictionary.Item.StartsWith(e.NewTextValue) || z.transcription.TranscriptionItem.StartsWith(e.NewTextValue) || z.dictionaryRu.ItemRu.StartsWith(e.NewTextValue)));
-				}
-			}
+                recentlyViewed.IsVisible = !recentlyViewed.IsVisible;
+                dictionatyView.ItemsSource = null;
+                if (searchBar.Text != "")
+                    dictionatyView.ItemsSource = (await App.Database.GetMeaningWithChildren()).Where(z => z.dictionary.fkLanguage == 1 && (z.dictionary.Item.StartsWith(e.NewTextValue) || z.transcription.TranscriptionItem.StartsWith(e.NewTextValue) || z.dictionaryRu.ItemRu.StartsWith(e.NewTextValue)));
+            }
+            
 		}
+
+        private async void dictionatyViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+			Meaning selected = (Meaning)e.SelectedItem;
+			await Navigation.PushAsync(new WordPage(selected));
+        }
     }
 }
---
+++ linguid/Views/ContentMainPage/WordPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace linguid.Views.ContentMainPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WordPage : ContentPage
+    {
+        Meaning meaning = new Meaning();
+        public WordPage(Meaning selected)
+        {
+            InitializeComponent();
+            meaning = selected;
+
+            item.Text = selected.dictionary.Item;
+            itemRu.Text = selected.dictionaryRu.ItemRu;
+            transcription.Text = selected.transcription.TranscriptionItem;
+
+            var userLogin = Thread.CurrentPrincipal.Identity.Name;
+            if (userLogin == "")
+            {
+                addFav.IsVisible = false;
+                delFav.IsEnabled = false;
+            }
+            else
+            {
+                addFav.IsVisible = true;
+            }
+        }
+
+        protected override async void OnAppearing()
+        {
+            await App.Database.CreateMbC();
+            await App.Database.CreateMeaning();
+
+            categoryView.ItemsSource = (await App.Database.GetMbCWithChildren()).Where(z => z.fkMeaning == meaning.MeaningID);
+
+            base.OnAppearing();
+        }
+
+        private async void categoryViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var userLogin = Thread.CurrentPrincipal.Identity.Name;
+            if (userLogin == "")
+            {
+                categoryView.IsEnabled = false;
+            }
+            else
+            {
+                MeaningByCategory selected = (MeaningByCategory)e.SelectedItem;
+                Category category = selected.category;
+                await Navigation.PushAsync(new StartLessonPage(category));
+            }      
+        }
+    }
+}
