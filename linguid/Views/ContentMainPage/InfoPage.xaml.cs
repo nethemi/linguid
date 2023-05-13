c0c3031d7163b230caa531a19071ffdff92b931d
Анжела Санникова <sannikova.anzhela.01@outlook.com>
2023-05-13 09:56:22 +0700

    list

--- linguid/Views/ContentMainPage/InfoPage.xaml.cs
+++ linguid/Views/ContentMainPage/InfoPage.xaml.cs
@@ -12,11 +12,14 @@ namespace linguid.Views.ContentMainPage
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class InfoPage : ContentPage
     {
+        List<int> count = new List<int>() { 3, 3, 3, 4, 4, 2, 2, 5, 3, 2, 2, 4};
         public InfoPage()
         {
             InitializeComponent();
 
             db.Text = App.DATABASE_NAME;
+            countView.ItemsSource = count;
+            
         }
 
         protected override async void OnAppearing()
